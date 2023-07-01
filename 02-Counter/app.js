1946a35034d0ef081a0039df58a38cd4d6079391
Soul-331 <khagendrakvk@gmail.com>
2023-06-30 21:14:56 -0700

    Counter

---
+++ 02-Counter/app.js
@@ -0,0 +1,33 @@
+//set initial count
+let count=0;
+
+//select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+// const btn_arr = Array.from(btns);
+
+btns.forEach(element=> {
+    element.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        if(count > 0){
+            value.style.color='green';
+        }
+        if(count < 0){
+            value.style.color='red';
+        }
+        if(count===0){
+            value.style.color='#222'
+        }
+        value.textContent = count;
+    });
+});
